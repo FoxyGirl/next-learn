196aca8d3a9c4bf524b868ef419d5ed8abe7ea69
Elena Pavlova <lenlho22@gmail.com>
2021-01-26 07:55:12 +0300

    Add props with getStaticProps

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,28 @@
-const Blog = ({ theme }) => {
+import { pageVerify } from "../helpers/pageVerify";
+
+export const getStaticProps = () => {
+  const protectedPage = pageVerify("blog");
+
+  return {
+    props: {
+      protectedPage,
+    },
+  };
+};
+
+const Blog = ({ theme, protectedPage }) => {
   console.log("Blog render");
+
+  const protectedPageJSX = protectedPage && (
+    <h2 style={{ color: "red" }}>This is protected page</h2>
+  );
 
   return (
     <>
       <h1>Blog page</h1>
       <p>This is the Blog page!</p>
       <p>Current Theme: {theme}</p>
+      {protectedPageJSX}
     </>
   );
 };
